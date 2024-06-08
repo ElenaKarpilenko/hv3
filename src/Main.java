e9e01d82176ea1fbed8e64b17759c5008ce64529
elenakarpilenko <elenakarpilenko787@gmail.com>
2024-06-08 19:55:46 +0600

    дз 3

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,49 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Создаем массив из 15 элементов
+        double[] array = new double[15];
+        /*Создать массив дробных (не целых) чисел размером 15 элементов и наполнить его положительными и отрицательными числами, первые 2 числа в массиве должны быть положительными;
+        Затем через 1 цикл “for each” посчитать среднее арифметическое положительных чисел? расположенных после первого отрицательного числа и вывести его на экран (8, -2, -4, 2, 3, 6, -7) =  11/3
+        // Заполняем массив положительными и отрицательными числами
+        array[0] = 8.0;
+        array[1] = 2.0;
+        array[2] = -4.0;
+        array[3] = 2.0;
+        array[4] = 3.0;
+        array[5] = 6.0;
+        array[6] = -7.0;
+        array[7] = 1.0;
+        array[8] = -3.0;
+        array[9] = 4.0;
+        array[10] = 5.0;
+        array[11] = -6.0;
+        array[12] = 7.0;
+        array[13] = -8.0;
+        array[14] = 9.0;*/
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Переменные для подсчета суммы и количества положительных чисел
+        double sum = 0;
+        int count = 0;
+
+        // Флаг для определения первого отрицательного числа
+        boolean foundFirstNegative = false;
+
+        // Цикл for-each для обработки массива
+        for (double num : array) {
+            if (foundFirstNegative && num > 0) {
+                sum += num;
+                count++;
+            }
+            if (num < 0) {
+                foundFirstNegative = true;
+            }
         }
+
+        // Вычисление среднего арифметического
+        double average = (count > 0) ? sum / count : 0;
+
+        // Вывод результата на экран
+        System.out.println("Среднее арифметическое положительных чисел после первого отрицательного: " + average);
     }
 }
